204b5952a0f16c45e1d085e75be0f78380d37864
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-01-19 10:32:39 -0500

    Added Data State

--- MusicPlayer/music-player/src/App.js
+++ MusicPlayer/music-player/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles/app.scss';
 //Adding Components
 import Song from './components/Song.js';
 import Player from './components/Player.js';
+//Import Util
+import data from './util';
 
 function App() {
+  //State 
+  const[songs, setSongs] = useState(data());
+  const[currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong}/>
       <Player />
+
     </div>
   );
 }
