2b6a40d6f043404f8ea4e016c5770182b5a9b632
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-05-03 20:47:48 -0400

    Created GlobalStyles

--- Capture-Portfolio/capture/src/App.js
+++ Capture-Portfolio/capture/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+//Import GlobalStyle
+import GlobalStyle from './components/GlobalStyled';
 //Import Pages
 import AboutUs from './pages/AboutUs';
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
