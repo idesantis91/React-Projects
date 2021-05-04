9e003067a0c5e7b9ef1d7cdd84b1f29d97440cdd
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-05-03 20:09:06 -0400

    Created ServicesSection

--- Capture-Portfolio/capture/src/App.js
+++ Capture-Portfolio/capture/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import AboutSection from './components/AboutSection';
 //Import Pages
 import AboutUs from './pages/AboutUs';
 function App() {
   return (
     <div className="App">
-      <AboutSection />
+      <AboutUs />
     </div>
   );
 }
