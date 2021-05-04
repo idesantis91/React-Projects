fac55c6caa8a2482e4bd265d847dd9baf83e9009
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-05-03 20:16:16 -0400

    Created FaqSection

---
+++ Capture-Portfolio/capture/src/components/FaqSection.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const FaqSection = () =>{
+
+    return(
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="questions">
+                <h4>How Do I Start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipidicing
+                        Necessitatibus, neque.
+                    </p>
+                </div>
+            </div>
+            <div className="questions">
+                <h4>Daily Schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipidicing
+                        Necessitatibus, neque.
+                    </p>
+                </div>
+            </div>
+            <div className="questions">
+                <h4>Different Payment Methods</h4>
+                <div className="answer">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipidicing
+                        Necessitatibus, neque.
+                    </p>
+                </div>
+            </div>
+            <div className="questions">
+                <h4>What Products do you offer?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipidicing
+                        Necessitatibus, neque.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection;
