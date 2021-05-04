1b93cd7c2b62f07b5f8d0427140072f57e933c1f
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-05-04 17:10:09 -0400

    Created ServicesSection Styles

--- Capture-Portfolio/capture/src/components/AboutSection.js
+++ Capture-Portfolio/capture/src/components/AboutSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import home1 from '../img/home1.png';
 //Styled
 import styled from 'styled-components';
-
+import {About, Description, Image, Hide} from '../styles';
 const AboutSection = () =>{
 
     return(
@@ -13,7 +13,7 @@ const AboutSection = () =>{
                         <h2>We work to make</h2>
                     </Hide>
                     <Hide>
-                        <h2>your <span>dreams</span> come </h2>
+                        <h2>your <span>dreams</span> come</h2>
                     </Hide>
                     <Hide>
                         <h2>true.</h2>
@@ -30,39 +30,5 @@ const AboutSection = () =>{
         </About>
     );
 };
-
-//Styled Component
-
-const About = styled.div`
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: white;
-`;
-
-const Description = styled.div `
-    flex:1;
-    padding-right: 5rem;
-
-    h2{
-        font-weight: lighter;
-    }
-`;
-
-const Image = styled.div `
-    flex:1;
-    overflow: hidden;
-    img{
-        width:100%;
-        height: 80vh;
-        object-fit: cover;
-    }
-`;
-
-const Hide = styled.div `
-    overflow: hidden;
-`;
 
 export default AboutSection;
