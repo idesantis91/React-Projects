00c4635e80e83c44c20a6afac8d3ec6e159f8b5e
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-05-04 20:43:41 -0400

    Created ReactRouter

--- Capture-Portfolio/capture/src/App.js
+++ Capture-Portfolio/capture/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 //Import GlobalStyle
 import GlobalStyle from './components/GlobalStyled';
+import Nav from './components/Nav';
 //Import Pages
 import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav /> 
+      <Switch>  
+        <Route path="/" exact>
+       <AboutUs/>
+      </Route> 
+      <Route path="/work">
+        <OurWork/>
+      </Route>
+      <Route path="/contact">
+        <ContactUs/>
+      </Route>    
+      </Switch>
     </div>
   );
 }
---
+++ Capture-Portfolio/capture/src/pages/ContactUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ContactUs = () =>{
+
+    return(
+        <div>
+            <h1>Contact</h1>
+        </div>
+    )
+
+}
+
+export default ContactUs;
