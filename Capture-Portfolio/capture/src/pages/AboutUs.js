04fafbe48194c52546839683297daa579b9ed049
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-05-07 17:44:45 -0400

    Animate.js added

--- Capture-Portfolio/capture/src/pages/AboutUs.js
+++ Capture-Portfolio/capture/src/pages/AboutUs.js
@@ -3,16 +3,17 @@ import React from 'react';
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
 import FaqSection from '../components/FaqSection';
-
+//Animations
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
 const AboutUs = () =>{
 
-    return( <>
+    return( <motion.div variants={pageAnimation} inital='hidden' animate='show'>
         <AboutSection/>
         <ServicesSection />
         <FaqSection />
-    </>
+    </motion.div>
     );
-    
 };
 
 export default AboutUs;
